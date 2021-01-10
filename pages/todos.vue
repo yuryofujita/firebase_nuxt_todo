f640cc20535dd86f3d72ea61dea56003630137e9
yuryofujita <dasman74@hotmail.com>
2021-01-10 10:51:32 +0900

    first commit

---
+++ pages/todos.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <!-- {{ todos }} -->
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">
+        <!-- {{ todo }} -->
+        <span v-if="todo.created">
+          <input 
+          type="checkbox"
+          v-bind:checked="todo.done"
+          @change="toggle(todo)">
+          <span v-bind:class="{ done: todo.done }">
+            {{ todo.name }} {{ todo.created.toDate() | dateFilter }}
+          </span>
+          <button v-on:click="remove(todo.id)">X</button>
+          <!-- {{ todo.id }} -->
+        </span>
+      </li>
+    </ul>
+    <div class="form">
+      <form v-on:submit.prevent="add">
+        <input v-model="name">
+        <button>Add</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+  import moment from 'moment'
+  export default {
+    data: function() {
+      return {
+        name: '',
+        done: false
+      }
+    },
+    created: function() {
+      this.$store.dispatch('todos/init')
+    },
+    methods: {
+      add() {
+        this.$store.dispatch('todos/add', this.name)
+        this.name = ''
+      },
+      remove(id) {
+        this.$store.dispatch('todos/remove', id)
+      },
+      toggle(todo) {
+        this.$store.dispatch('todos/toggle', todo)
+      }
+    },
+    computed: {
+      todos() {
+        return this.$store.state.todos.todos
+        // return this.$store.getters['todos/orderdTodos']
+      }
+    },
+    filters: {
+      dateFilter: function(date) {
+        return moment(date).format('YYYY/MM/DD HH:mm:ss')
+      }
+    }
+  }
+</script>
+
+<style>
+li > span > span.done {
+  text-decoration: line-through;
+}
+</style>
---
+++ store/todos.js
@@ -0,0 +1,39 @@
+import firebase from '~/plugins/firebase'
+import { firestoreAction } from 'vuexfire'
+import { _ } from 'core-js'
+
+const db = firebase.firestore()
+const todosRef = db.collection('todos')
+
+export const state = () => ({
+    todos: []
+})
+
+export const actions = {
+    init: firestoreAction(({ bindFirestoreRef }) => {
+        bindFirestoreRef('todos', todosRef)
+    }),
+    add: firestoreAction((context, name) => {
+        if (name.trim()) {
+            todosRef.add({
+                name: name,
+                done: false,
+                created: firebase.firestore.FieldValue.serverTimestamp()
+            })
+        }
+    }),
+    remove: firestoreAction((context, id) => {
+        todosRef.doc(id).delete()
+    }),
+    toggle: firestoreAction((context, todo) => {
+        todosRef.doc(todo.id).update({
+            done: !todo.done
+        })
+    })
+}
+
+// export const getters = {
+//     orderdTodos: state => {
+//         return _.sortBy(state.todos, 'created')
+//     }
+// }
